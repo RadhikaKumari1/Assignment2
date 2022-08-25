c6599ab954b0fd98a2a4b4600a607d23f2ca3f79
Radhika <Radhika@DESKTOP-S0V7M4T>
2022-08-25 18:20:23 +0530

    first commit

---
+++ sort/src/sort/SortInAscendingOrder.java
@@ -0,0 +1,47 @@
+package sort;
+import java.util.*;
+public class SortInAscendingOrder {
+
+
+		
+		
+		static void sort(String[]s, int n)
+		{
+			for(int i=1; i<n; i++) {
+				
+				String temp = s[i];
+				
+				int j=i-1;
+				while(j>=0 && temp.length()<s[j].length())
+				{
+					s[j+1]=s[j];
+					j--;
+				}
+				s[j+1]=temp;
+			}
+		}
+		
+		static void printArrayString(String str[], int n)
+		{
+			for(int i=0; i<n; i++)
+				System.out.print(str[i]+" ");
+		
+				}
+				
+		public static void main(String args[])
+		{
+			String[]arr = {"This", "is", "a", "easiest", "example"};
+			int n = arr.length();
+			
+			sort(arr,n);
+			
+			printArrayString(arr,n);
+			
+			}
+			
+		
+	
+
+	}
+
+
